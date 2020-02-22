c733af74c6b79f5223a7914936928f817ed79be6
殷小林 <yinxiaolin@inke.cn>
2020-02-22 16:06:03 +0800

    680

---
+++ LeetCode/lc680_ValidPalindrome2.cpp
@@ -0,0 +1,48 @@
+/*20200221 balis
+Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+Example 1:
+Input: "aba"
+Output: True
+
+Example 2:
+Input: "abca"
+Output: True
+Explanation: You could delete the character 'c'.
+ 
+Note:
+The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+*/
+
+#include<iostream>
+using namespace std;
+
+bool isPalindrome(string str, int l, int r) ;
+int main(){
+    string str;
+    cin >> str;
+    int l=0, r=str.length() -1;
+
+    while(l <r) {
+        if(str[l] != str[r]) {
+            cout << (isPalindrome(str, l+1, r) || isPalindrome(str, l, r-1)) << endl;
+            return 0;
+        }
+        l++;
+        r--;
+    }
+
+    cout << true << endl;
+    return 0;
+}
+
+bool isPalindrome(string str, int l, int r) {
+    while(l < r) {
+        if (str[l] != str[r]) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
+}
